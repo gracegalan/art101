8a227b2c00531237399b6e8e004906f4e7044f44
gracegalan <grace.galan.e@gmail.com>
2023-10-22 22:53:32 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,12 +1,12 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 - Pseudocoding
+// Author: Grace Galan and Ashley Huynh
+// Date: 22 October 2023
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+// this is an example function and this comment tells what it does and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
@@ -19,3 +19,16 @@ function main() {
 
 // let's get this party started
 main();
+
+// initialize the grid with empty cells
+// set up two players, each with a set of pieces
+// Main game loop:
+  // display the current state of the grid
+  // ask the current player for their move and chosen column
+  // if the move is valid:
+    // place the player's token in the specified column on the board
+    // check for a win
+    // check for a tie 
+    // switch to the next player
+// display the winner or declare a draw
+
