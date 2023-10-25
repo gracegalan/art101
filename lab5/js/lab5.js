0b707578f913a190051415e0183f987455011ff0
gracegalan <grace.galan.e@gmail.com>
2023-10-25 07:59:28 -0700

    lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,37 @@
+// index.js - Car Variables
+// Author: Grace Galan
+// Date: 24 October 2023
+
+//Define Variables
+var make= "honda";
+var model= "civic";
+var color= "silver";
+var year= 2005;
+
+//Calculate
+var age= 2023 - year; 
+
+// output
+document.writeln("make: " + make + "<br>"); 
+document.writeln("model: "+ model + "<br>" ); 
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + "years<br>")
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
