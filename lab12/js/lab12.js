bacd2f1767ad2f878377bc0733cea1ee3046038c
gracegalan <grace.galan.e@gmail.com>
2023-11-22 07:53:36 -0800

    lab 12

---
+++ lab12/js/lab12.js
@@ -0,0 +1,16 @@
+//Lab 12 - Conditionals
+//Author: Grace Galan
+//Date: 11.21.23
+
+//Completed with the assistance of Wes Modes' lab template and partner Ren Murphey
+
+
+//make sure button works
+$("#button").click(function() {
+
+    console.log("Sort");
+
+});
+
+
+
